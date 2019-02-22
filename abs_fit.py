505dd4d9661d2170513f73aaef09a47b461ce518
宮田　聖也 <miyataseiya@miyatasrinoiMac.wifi.lns.tohoku.ac.jp>
2019-02-22 18:05:03 +0900

    save.

--- abs_fit.py
+++ abs_fit.py
@@ -10,17 +10,18 @@ def abs_fit2(x, p1, p2):
   return p1 + np.abs(x-p2)
 
 #date = 19012311
-date = 19013016
-numl = 2130
-numr = 2177
-datpath = 't0.dat'
-datpath2 = 't0_a.dat'
+#date = 19013117
+date = 19022217
+numl = 5030
+numr = 5272
+datpath = 't0_18a.dat'
+datpath2 = 't0_18a_er.dat'
 for num in range(numl, numr):
-    if(os.path.exists("../eps/{0}/slicet0/dat/slicefitt0-fit{1}.dat".format(date, num)) == False):
+    if(os.path.exists("../eps/{0}/hist/slicet0/dat/slicefitt0-fit{1}.dat".format(date, num)) == False):
         continue
-    data_p0, data_p1, data_p2 = np.loadtxt("../eps/{0}/slicet0/dat/slicefitt0-fit{1}.dat".format(date, num), comments="!", unpack=True)
-    data_x = np.loadtxt("../eps/{0}/slicet0/dat/slicefitt0-xaxis{1}.dat".format(date, num), comments="!", unpack=True)
-    if(len(data_p0)<4):
+    data_p0, data_p1, data_p2 = np.loadtxt("../eps/{0}/hist/slicet0/dat/slicefitt0-fit{1}.dat".format(date, num), comments="!", unpack=True)
+    data_x = np.loadtxt("../eps/{0}/hist/slicet0/dat/slicefitt0-xaxis{1}.dat".format(date, num), comments="!", unpack=True)
+    if(np.size(data_p0)<4):
         continue
     fig = plt.figure(figsize=(10,6))
     ax = fig.add_subplot(111)
@@ -41,16 +42,18 @@ for num in range(numl, numr):
 
     ferr = np.sqrt(np.diag(cov))
     with open(datpath, mode='a') as f:
-        f.write('\n{0} {1} {2}'.format(num-2000, param[0], ferr[0]))
+        f.write('\n{0} {1} {2}'.format(num-5000, param[0], ferr[0]))
     
     ferr2 = np.sqrt(np.diag(cov2))
     with open(datpath2, mode='a') as f:
-        f.write('\n{0} {1} {2}'.format(num-2000, param2[0], ferr2[0]))
+        f.write('\n{0} {1} {2}'.format(num-5000, param2[0], ferr2[0]))
+
+
      
     plt.plot(x, y)
     plt.plot(x, y2)
     plt.errorbar(data_x, data_p1, yerr=data_p2, fmt='ro', ecolor='g')
-    hum = num - 2000
+    hum = num - 5000
     plt.annotate('t\' = p0 + p1*|t-p2|' ,xy=(-3650, -3670))
     plt.xlabel("t_Fw{0},{1}-t_Fp".format(hum-1, hum+1))
     plt.ylabel("t_Fw{0}-t_Fp".format(hum))
--- t0_plot.py
+++ t0_plot.py
@@ -6,26 +6,27 @@ import os
 def fitfunc(x, p1, p2, p3):
     return p1 + p2*x + p3*x*x
 
-data_wire, data_t0, data_t0err= np.loadtxt("t0.dat", comments="!", unpack=True)
+data_wire, data_t0, data_t0err= np.loadtxt("t0_temp3.dat", comments="!", unpack=True)
 fig = plt.figure(figsize=(10,6))
 ax = fig.add_subplot(111)
 ax.hold(True)
 ax.plot(data_wire, data_t0, "x", color="k", label="x-t")
-ax.set_xlim(50, 95)
+ax.set_xlim(0, 272)
 ax.set_ylim(-3770.,-3740.)
 
 plt.errorbar(data_wire, data_t0, yerr=data_t0err, fmt='bo', ecolor='r')
 #plt.errorbar(data_x, data_p1, yerr=data_p2, fmt='ro', ecolor='g')
-x = np.arange(50, 90, 0.01)
+x = np.arange(0, 272, 0.01)
 para_ini = [ -3800, 0, 1 ]
-param, cov = curve_fit(fitfunc, data_wire, data_t0, para_ini)
-y = param[0] + param[1]*x + param[2]*x*x
+#param, cov = curve_fit(fitfunc, data_wire, data_t0, para_ini)
+#y = param[0] + param[1]*x + param[2]*x*x
 
-data_wire2, data_t02, data_t0err2= np.loadtxt("t0_a.dat", comments="!", unpack=True)
+data_wire2, data_t02, data_t0err2= np.loadtxt("t0_a_temp3.dat", comments="!", unpack=True)
 ax.plot(data_wire2, data_t02, "x", color="r")
 plt.errorbar(data_wire2, data_t02, yerr=data_t0err2, fmt='go', ecolor='r')
-plt.plot(x, y)
+
+#plt.plot(x, y)
 
 plt.xlabel("Fw")
 plt.ylabel("p0")
-plt.savefig("pdf/num_vs_t0.pdf")
+plt.savefig("pdf/num_vs_t0_pro_18a.pdf")
